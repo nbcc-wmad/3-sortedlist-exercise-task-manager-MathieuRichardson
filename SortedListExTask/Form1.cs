33605516389b91f05510b67c32f6d32c3c41db39
MathieuRichardson <MathieuRichardson1999@gmail.com>
2019-04-30 14:50:37 -0300

    progress

--- SortedListExTask/Form1.cs
+++ SortedListExTask/Form1.cs
@@ -10,11 +10,62 @@ using System.Windows.Forms;
 
 namespace SortedListExTask
 {
+    public struct Task
+    {
+        public string Date;
+        public string TaskName;
+
+    }
     public partial class Form1 : Form
     {
+        public Task task = new Task();
+        public List<Task> allTasks = new List<Task>();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+        private void loadTasks()
+        {
+            //value member stuff?
+
+            lstTasks.Items.Clear();
+            foreach (var t in allTasks)
+            {
+                lstTasks.Items.Add(task.Date);
+
+            }
+            
+
+            //lstTasks.DisplayMember = "Date";
+            //lstTasks.DataSource = allTasks;
+            //lstTasks.ValueMember = "TaskName";
+
+
+
+        }
+        private void printTaskName(int? ordinal = null)
+        {
+            var t = allTasks[ordinal.GetValueOrDefault()];
+            lblTaskDetails.Text = t.TaskName;
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            task.Date = dtpTaskDate.Value.ToString();
+            task.TaskName = txtTask.Text.Trim();
+            allTasks.Add(task);
+            loadTasks();
+
+           //foreach(var t in allTasks)
+           // {
+           //     MessageBox.Show($"{task.Date} {task.TaskName}");
+           // }
+        }
+
+        private void lstTasks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            printTaskName(lstTasks.SelectedIndex);
         }
     }
 }
